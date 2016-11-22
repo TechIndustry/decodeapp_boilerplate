0b6829397d1ca62fa1eb0790592e519fc6541568
clydeinwebdev <clydeinwebdev@gmail.com>
2016-11-22 13:19:19 +0800

    bootstrap and materialize css ok

--- public/js/app.js
+++ public/js/app.js
@@ -823,11 +823,11 @@ component.recipeLayout();
 /*
 "use strict";
 
-
 class App{
 	constructor(){
 		this.movies = [
 			{
+				"id": 1,
 				"Title":"Winter Is Coming",
 				"Year":"2011",
 				"Director":"Timothy Van Patten",
@@ -835,6 +835,7 @@ class App{
 				"Actors":"Sean Bean, Mark Addy, Nikolaj Coster-Waldau, Michelle Fairley"
 			},
 			{
+				"id": 2,
 				"Title":"The North Remembers",
 				"Year":"2012",
 				"Director":"Alan Taylor",
@@ -842,6 +843,7 @@ class App{
 				"Actors":"Peter Dinklage, Lena Headey, Nikolaj Coster-Waldau, Michelle Fairley"
 			},
 			{
+				"id": 3,
 				"Title":"Valar Dohaeris",
 				"Year":"2013",
 				"Director":"Daniel Minahan",
@@ -849,6 +851,7 @@ class App{
 				"Actors":"Peter Dinklage, Lena Headey, Emilia Clarke, Kit Harington"
 			},
 			{
+				"id": 4,
 				"Title":"Two Swords",
 				"Year":"2014",
 				"Director":"D.B. Weiss",
@@ -856,6 +859,7 @@ class App{
 				"Actors":"Peter Dinklage, Nikolaj Coster-Waldau, Lena Headey, Emilia Clarke"
 			},
 			{
+				"id": 5,
 				"Title":"The Wars to Come",
 				"Year":"2015",
 				"Director":"Michael Slovis",
@@ -863,6 +867,7 @@ class App{
 				"Actors":"Peter Dinklage, Nikolaj Coster-Waldau, Lena Headey, Emilia Clarke"
 			},
 			{
+				"id": 6,
 				"Title":"The Red Woman",
 				"Year":"2016",
 				"Director":"Jeremy Podeswa",
@@ -911,21 +916,69 @@ class App{
 		// this.movies = dummy;
 		let details = document.getElementById('movieDetails');
 		details.innerHTML = "";
-		this.movieListInfo();		
+		
+		this.movieListInfo();	
+		this.showMovieList();	
 	}
 
 	updateMovie(key){
+		let t = document.getElementById('updateTitle');
 		let y = document.getElementById('updateYear');
 		let d = document.getElementById('updateDirector');
 		let a = document.getElementById('updateActors');
 
 		let m = this.movies[key];
-		let movie = {"Title":m.Title,"Year":y.value,"Director":d.value,"Poster":m.Poster,"Actors":a.value};
+		let movie = {"id":m.id,"Title":t.value,"Year":y.value,"Director":d.value,"Poster":m.Poster,"Actors":a.value};
 
 		this.movies[key] = movie;
 		let details = document.getElementById('movieDetails');
 		details.innerHTML = "";
-		this.movieListInfo();			
+		
+		this.movieListInfo();
+		this.showMovieList();
+	}
+
+	showLandingPage(){
+		$('#landingpage').show();
+		$('#createpage').hide();
+		$('#readpage').hide();
+		$('#updatedeletepage').hide();
+	}	
+
+	showMovieList(){
+		$('#landingpage').hide();
+		$('#createpage').hide();
+		$('#readpage').show();
+		$('#updatedeletepage').hide();
+	}
+
+	showMovieCreate(){
+		$('#landingpage').hide();
+		$('#createpage').show();		
+		$('#readpage').hide();
+		$('#updatedeletepage').hide();
+	}
+
+	showUpdateDelete(){
+		$('#landingpage').hide();
+		$('#createpage').hide();		
+		$('#readpage').hide();
+		$('#updatedeletepage').show();
+	}	
+
+	searchMovie(value=""){
+		let objects = [];
+		let r = this.movies;
+		for(let i=0;i<r.length;i++){
+			// console.log("r:",r[i].Title.toUpperCase().indexOf(title.toUpperCase()));
+			let expr1 = (r[i].Title.toUpperCase().indexOf(value.toUpperCase()) > -1);
+			let expr2 = (r[i].Year.toUpperCase().indexOf(value.toUpperCase()) > -1);
+			// console.log(name," vs ",r[i].name," = ",expr);
+			if(expr1 || expr2){
+				objects.push(r[i]);
+			}
+		}
+		return objects;		
 	}
 }
 
@@ -936,12 +989,79 @@ class Component extends App{
 
 	movieList(){
 		this.render(
-			`
-				<div class="row">
-					<div class="col col-sm-6">
+			`	      
+		<div class="header clearfix">
+            <nav>
+              <ul class="nav nav-pills float-xs-right">
+                <li class="nav-item">
+                  <a class="nav-link" href="#" onclick="component.showLandingPage()">
+					<i class="fa fa-home"></i>
+                  	Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link " href="#" onclick="component.showMovieList()">
+					<i class="fa fa-list"></i>
+                  	Movie List</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" onclick="component.showMovieCreate()">
+					<i class="fa fa-film"></i>
+                  	Movie Create</a>
+                </li>
+              </ul>
+            </nav>            
+            <h3 class="text-muted">
+            	<img class="decode-logo" src="img/decode.png"  />
+            	Movie App
+            </h3>
+          </div>
+
+
+		<div id="landingpage">
+          <div class="jumbotron">
+            <h1 class="display-3">Movie App</h1>
+            <p class="lead">A simple decode demo app about movie.</p>
+            <i class="fa fa-film fa-5x red"></i>
+          </div>
+
+          <div class="row marketing">
+            <div class="col-lg-6">              
+              <h4>
+              	<i class="fa fa-gears green"></i>
+              	Create Movie</h4>
+              <p>Provides an interface to allow adding new movie to the list. </p>
+
+              <h4>
+				<i class="fa fa-gears green"></i>
+              	Read Movie</h4>
+              <p>Provides a list view that supports movie search and show details features.</p>
+
+            </div>
+
+            <div class="col-lg-6">
+              <h4>
+              	<i class="fa fa-gears green"></i>
+              	Update Movie</h4>
+              <p>Allows the user to modify the content of a particular movie.</p>
+
+              <h4>
+              	<i class="fa fa-gears green"></i>
+              	Delete Movie</h4>
+              <p>Allows the user to remove a particular movie object from the list.</p>
+
+            </div>
+          </div>          
+
+		</div>
+				
+				<div id="createpage" class="row marketing">
+					<div class="col col-sm-12">
 						<div id="movieCreate"></div>						
 					</div>
-					<div class="col col-sm-6">
+				</div>
+
+				<div id="readpage" class="row marketing">
+					<div class="col col-sm-12">
 						<h1>Movie List</h1>
 						<table id="movieList" class="table">
 							<thead>
@@ -951,20 +1071,43 @@ class Component extends App{
 									<th>Action</th>
 								</tr>
 							</thead>
+							<div class="form-group">
+							    <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
+							    <div class="input-group">
+							      <div class="input-group-addon">
+									  <span class="fa fa-search"></span>
+									  <!-- checkout other fontawesome icons at http://fontawesome.io/icons/ -->
+							      </div>
+							      <input oninput="component.movieListInfo(this.value)" type="text" class="form-control" placeholder="Search">
+							    </div>
+							  </div>
 							<tbody id="movieListInfo"></tbody>
 						</table>
 					</div>
 				</div>
-				<div id="movieDetails"></div>
+
+				<div id="updatedeletepage" class="row marketing">
+					<div id="movieDetails"></div>
+				</div>
+				
+          <footer class="footer">
+            <p>&copy; decode 2016</p>
+          </footer>
+
 			`
 			,document.getElementById('app'));
 		this.movieListInfo();
+		$('#landingpage').show();
+		$('#createpage').hide();		
+		$('#readpage').hide();
 	}
 
-	movieListInfo(){
+	movieListInfo(filter){
+		// console.log(filter);
 		let html = "";
-		let m = this.movies;
-		for(let i=0;i<m.length;i++){
+		// let m = this.movies;
+		let m = this.searchMovie(filter);
+		for(let i=0;i<m.length;i++){	
 			html += `
 				<tr>
 					<td>${m[i].Title}</td>
@@ -993,15 +1136,17 @@ class Component extends App{
 						Actors: ${this.movies[key].Actors}<br/>
 						<button class="btn btn-success" onclick="component.movieUpdate(${key})">Update</button>
 						<button class="btn btn-danger" onclick="component.deleteMovie(${key})">Delete</button>
+						<button class="btn btn-info" onclick="component.showMovieList()">Back</button>
 					</div>	
 				</div>			
 			`,document.getElementById('movieDetails'));
+			this.showUpdateDelete();
 	}
 
 	movieCreate(){
 		this.render(
 			`
-				<h1>New Movie</h1>
+				<h1>Movie Create</h1>
 				Title: <input class="form-control" id="newTitle" type="" placeholder="Enter Title" /><br/>
 				Year: <input class="form-control" id="newYear" type="" placeholder="Enter Title" /><br/>
 				Director: <input class="form-control" id="newDirector" type="" placeholder="Enter Director" /><br/>
@@ -1014,13 +1159,47 @@ class Component extends App{
 	movieUpdate(key){
 		this.reRender(
 			`
-					<h4 class="media-heading">${this.movies[key].Title}</h4>
-			        Year: <input id="updateYear" type="text" value="${this.movies[key].Year}" /><br/>
-					Director: <input id="updateDirector" type="text" value="${this.movies[key].Director}" /><br/>
-					Actors: <input id="updateActors" type="text" value="${this.movies[key].Actors}" /><br/>
-					<button class="btn btn-success" onclick="component.updateMovie(${key})">Save</button>
+				<div class="input-group input-group-md">
+		        	<span class="input-group-addon">
+		        		<span>ID</span>
+		        	</span>
+		        	<input readonly class="form-control" type="text" value="${this.movies[key].id}" /><br/>
+		        </div>
+		        <br/>
+		        <div class="input-group input-group-md">
+		        	<span class="input-group-addon">
+		        		<span>Title</span>
+		        	</span>
+		        	<input class="form-control" id="updateTitle" type="text" value="${this.movies[key].Title}" /><br/>
+		        </div>
+		        <br/>
+		        <div class="input-group input-group-md">
+		        	<span class="input-group-addon">
+		        		<span>Year</span>
+		        	</span>
+		        	<input class="form-control" id="updateYear" type="text" value="${this.movies[key].Year}" /><br/>
+		        </div>
+		        <br/>
+		        <div class="input-group input-group-md">
+		        	<span class="input-group-addon">
+		        		<span>Director</span>
+		        	</span>
+		        	<input class="form-control" id="updateDirector" type="text" value="${this.movies[key].Director}" /><br/>
+		        </div>	
+		        <br/>
+		        <div class="input-group input-group-md">
+		        	<span class="input-group-addon">
+		        		<span>Actors</span>
+		        	</span>
+		        	<input class="form-control" id="updateActors" type="text" value="${this.movies[key].Actors}" /><br/>
+		        </div>	
+				<br/>
+				<button class="btn btn-success btn-block" onclick="component.updateMovie(${key})">Save</button>
 			`,document.getElementById('movieDetailsInfo'));
 	}
+
+
+	
 }
 
 let component = new Component();
